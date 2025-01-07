a0bb0e469bff1999563c91a64f81cef34dfefeda
CodeDocManas <manas.bhattarai007@gmail.com>
2025-01-06 22:19:34 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -37,7 +37,7 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df): 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
     return X_train, X_test, y_trian, y_test 
     
